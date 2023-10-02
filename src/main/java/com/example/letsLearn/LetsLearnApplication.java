f66b30cc1d6ce9668b59d4a97a7bfd3ef4ba2373
Mert Can Akmızrak <mertcanakmizrak@gmail.com>
2023-10-02 11:21:39 +0300

    main

---
+++ src/main/java/com/example/letsLearn/LetsLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.letsLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsLearnApplication.class, args);
+	}
+
+}
